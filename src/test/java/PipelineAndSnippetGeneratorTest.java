82ea796d3cc1cd0bb187cf1438a5b7039b593215
MarenaHolt <fabriana.sands@gmail.com>
2022-06-12 20:05:18 +0500

    del Ignore

--- src/test/java/PipelineAndSnippetGeneratorTest.java
+++ src/test/java/PipelineAndSnippetGeneratorTest.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 

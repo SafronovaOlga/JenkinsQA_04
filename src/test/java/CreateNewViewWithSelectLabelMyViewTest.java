d43d6a9a0461da62cf35aabd24aaa23216435ad0
MaryMoroz <morozova.mary@gmail.com>
2022-06-17 14:44:24 -0400

    refactor6

--- src/test/java/CreateNewViewWithSelectLabelMyViewTest.java
+++ src/test/java/CreateNewViewWithSelectLabelMyViewTest.java
@@ -1,5 +1,4 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
@@ -8,7 +7,7 @@ public class CreateNewViewWithSelectLabelMyViewTest extends BaseTest {
     final String NAME_OF_VIEW = "My new view";
 
     @Test
-    public void testCreateNewView() throws InterruptedException{
+    public void testCreateNewView() {
         getDriver().findElement(By.xpath("//a[@title='New View']")).click();
         getDriver().findElement(By.id("name")).sendKeys(NAME_OF_VIEW);
         getDriver().findElement(By.xpath("//label[text()='My View']")).click();
@@ -22,10 +21,9 @@ public class CreateNewViewWithSelectLabelMyViewTest extends BaseTest {
         deleteCreatedView();
     }
 
-    private void deleteCreatedView() throws InterruptedException {
-        getDriver().findElement(By.xpath("//li/a[text()='Dashboard']")).click();
-        Thread.sleep(5000);
-        getWait20().until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='tab']/a[contains(@href, '/view/')]"))).click();
+    private void deleteCreatedView() {
+        getDriver().findElement(By.xpath("//ul[@id='breadcrumbs']/li[@class='children']")).click();
+        getDriver().findElement(By.xpath("//li/a[@href='/view/My%20new%20view/']")).click();
 
         getDriver().findElement(By.xpath("//a[@href='delete']")).click();
         getDriver().findElement(By.id("yui-gen1-button")).click();

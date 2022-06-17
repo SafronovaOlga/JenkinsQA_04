8e4a0bb48d9e365805071666d1029d2d2bb9958c
MaryMoroz <morozova.mary@gmail.com>
2022-06-17 14:48:55 -0400

    refactor7

--- src/test/java/CreateNewViewWithSelectLabelMyViewTest.java
+++ src/test/java/CreateNewViewWithSelectLabelMyViewTest.java
@@ -4,7 +4,16 @@ import org.testng.annotations.Test;
 import runner.BaseTest;
 
 public class CreateNewViewWithSelectLabelMyViewTest extends BaseTest {
-    final String NAME_OF_VIEW = "My new view";
+
+    private static final String NAME_OF_VIEW = "My new view";
+
+    private void deleteCreatedView() {
+        getDriver().findElement(By.xpath("//ul[@id='breadcrumbs']/li[@class='children']")).click();
+        getDriver().findElement(By.xpath("//li/a[@href='/view/My%20new%20view/']")).click();
+
+        getDriver().findElement(By.xpath("//a[@href='delete']")).click();
+        getDriver().findElement(By.id("yui-gen1-button")).click();
+    }
 
     @Test
     public void testCreateNewView() {
@@ -19,13 +28,5 @@ public class CreateNewViewWithSelectLabelMyViewTest extends BaseTest {
         );
 
         deleteCreatedView();
-    }
-
-    private void deleteCreatedView() {
-        getDriver().findElement(By.xpath("//ul[@id='breadcrumbs']/li[@class='children']")).click();
-        getDriver().findElement(By.xpath("//li/a[@href='/view/My%20new%20view/']")).click();
-
-        getDriver().findElement(By.xpath("//a[@href='delete']")).click();
-        getDriver().findElement(By.id("yui-gen1-button")).click();
     }
 }

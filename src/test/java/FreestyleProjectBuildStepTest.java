a0cf3f5a0dda14337045485d5adb8fd4ce3c495e
maratdp <mkaramyan.dp@gmail.com>
2022-06-19 13:28:34 +0300

    AT_007.001 | Freestyle project > Add a build step in configuration

--- src/test/java/FreestyleProjectBuildStepTest.java
+++ src/test/java/FreestyleProjectBuildStepTest.java
@@ -1,3 +1,8 @@
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -9,18 +14,18 @@ import org.testng.annotations.Test;
 import runner.BaseTest;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
-import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class FreestyleProjectBuildStepTest extends BaseTest {
 
     private static final String PROJECT_NAME = RandomStringUtils.randomAlphanumeric(3, 10);
 
-    private void createFreestyleProjectWithBuildStep() {
+    private void createFreestyleProjectWithBuildStep(String environmentVariable) throws InterruptedException, AWTException {
+        Robot robot = new Robot();
+
         getDriver().findElement(
-                By.xpath("//a[@class='model-link inside breadcrumbBarAnchor']")).click();
+                By.className("breadcrumbBarAnchor")).click();
         getDriver().findElement(By.linkText("New Item")).click();
         getDriver().findElement(By.id("name")).sendKeys(PROJECT_NAME);
         getDriver().findElement(
@@ -28,62 +33,59 @@ public class FreestyleProjectBuildStepTest extends BaseTest {
         getDriver().findElement(By.id("ok-button")).click();
         WebElement addBuildStep = getDriver().findElement(
                 By.xpath("//span[@class='yui-button yui-menu-button']//button[@type='button' and @suffix='builder']"));
+
         ((JavascriptExecutor) getDriver()).executeScript("arguments[0].scrollIntoView(true);", addBuildStep);
+
         new WebDriverWait(getDriver(), 10)
                 .until(ExpectedConditions.elementToBeClickable(addBuildStep));
+
         Actions action = new Actions(getDriver());
         action.moveToElement(addBuildStep);
         action.click().build().perform();
+
         getDriver().findElement(By.xpath("//a[text()='Execute shell']")).click();
 
-        getDriver().findElement(By.xpath("//div[@class='CodeMirror']//div[@class='CodeMirror-scroll cm-s-default']")).click();
+        Thread.sleep(2000);
 
-        /*getDriver().findElement(By.xpath("//div[@class='CodeMirror']//div[@class='CodeMirror-scroll cm-s-default']"))
-                .sendKeys("JOB_NAME=${JOB_NAME}\n" +
-                        "BUILD_NUMBER=${BUILD_NUMBER}");*/
+        WebElement variableField = getDriver().findElement(By.cssSelector("div .CodeMirror-lines > div > div:nth-child(5) > pre"));
+        variableField.click();
 
-        /*getDriver().findElement(By.xpath("//div[@class='CodeMirror-scroll cm-s-default Code-Mirror-focused']"))
-                .sendKeys("JOB_NAME=${JOB_NAME}\n" +
-                        "BUILD_NUMBER=${BUILD_NUMBER}");*/
+        StringSelection stringSelection = new StringSelection(environmentVariable);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(stringSelection, stringSelection);
 
-        /*getDriver().findElement(By.xpath("//div[starts-with(@class,'CodeMirror-scroll cm-s-default')]"))
-                .sendKeys("JOB_NAME=${JOB_NAME}\n" +
-                        "BUILD_NUMBER=${BUILD_NUMBER}");*/
+        robot.keyPress(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_CONTROL);
 
-        /*getDriver().findElement(By.xpath("//div[contains(@class,'CodeMirror-scroll cm-s-default')]"))
-                .sendKeys("JOB_NAME=${JOB_NAME}\n" +
-                        "BUILD_NUMBER=${BUILD_NUMBER}");*/
-
+        getDriver().findElement(
+                By.xpath("//button[contains(text(),'Apply')]")).click();
         getDriver().findElement(
                 By.xpath("//button[contains(text(),'Save')]")).click();
     }
 
-    private List<WebElement> getSuccessfulBuilds() {
-        return getDriver().findElements(
-                By.xpath("//td[@class='build-row-cell']//span/span/*[name()='svg' " +
-                        "and (contains(@tooltip, 'Success'))]"));
-    }
-
     @Test
-    public void testAddBuildStep_TC_007_001() {
-        createFreestyleProjectWithBuildStep();
-        assertEquals(getDriver().findElement(
-                        By.xpath("//h1[@class='job-index-headline page-headline']")).getText(),
-                "Project " + PROJECT_NAME);
-
+    public void testAddBuildStepToProject() throws InterruptedException, AWTException {
+        createFreestyleProjectWithBuildStep("JOB_NAME=${JOB_NAME}");
         getDriver().findElement(By.linkText("Build Now")).click();
-        getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         List<WebElement> buildHistory = getDriver().findElements(
-                By.xpath("//a[@class='tip model-link inside build-link display-name']"));
+                By.className("display-name"));
+
+        new WebDriverWait(getDriver(), 5)
+                .until(ExpectedConditions.visibilityOfAllElements(buildHistory));
 
         Actions action = new Actions(getDriver());
         action.moveToElement(buildHistory.get(0)).click().build().perform();
-        getDriver().findElement(By.xpath("//span[text()='Console Output']")).click();
 
-        WebElement consoleOutput = getDriver().findElement(By.xpath("//pre[@class='console-output']"));
-        assertTrue(consoleOutput.getText().contains("JOB_NAME"));
-        assertTrue(consoleOutput.getText().contains("BUILD_NUMBER"));
-        assertTrue(consoleOutput.getText().contains("Finished: SUCCESS"));
+        getDriver().findElement(By.xpath("//span[text()='Console Output']")).click();
+        WebElement consoleOutput = getDriver().findElement(By.className("console-output"));
+
+        new WebDriverWait(getDriver(), 5)
+                .until(ExpectedConditions.visibilityOf(consoleOutput));
+
+        assertTrue(consoleOutput.getText().contains("JOB_NAME=" + PROJECT_NAME));
+        assertTrue(consoleOutput.getText().contains("SUCCESS"));
     }
 }

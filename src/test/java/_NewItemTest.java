72ee3385fe1f658117109cc09ebec22b7c66e259
imperiya <darimper@gmail.com>
2022-06-21 13:44:55 -0400

    Tests moved to class _NewItemTest, Delete old class

--- src/test/java/_NewItemTest.java
+++ src/test/java/_NewItemTest.java
@@ -133,4 +133,16 @@ public class _NewItemTest extends BaseTest {
             Assert.assertTrue(icon.isEnabled());
         }
     }
+
+    @Test
+    public void testCheckBreadcrumbs() {
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        String title = getDriver().getTitle();
+        List<WebElement> breadcrumbs = getDriver().findElements(By.xpath("//ul[@id='breadcrumbs']/li"));
+
+        Assert.assertEquals(title, "New Item [Jenkins]");
+        Assert.assertEquals(breadcrumbs.get(0).getText(), "Dashboard");
+        Assert.assertEquals(breadcrumbs.get(2).getText(), "All");
+
+    }
 }
